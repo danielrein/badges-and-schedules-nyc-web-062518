3e37350ccff6bbfadb815d83eba86bfda1c95a68
Daniel Ran <danielr1735@gmail.com>
2018-06-19 13:10:16 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each do |name|
-    "Hello, #{name}! You'll be assigned to room #{attendees[name]}"
+  attendees.each_with_index do |name, index|
+    puts "Hello, #{name}! You'll be assigned to room #{attendees[index]}"
   end
 end
