35b6e00198cc53a887d303051d378b9f0e4add0b
Daniel Ran <danielr1735@gmail.com>
2018-06-19 13:06:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |name|
+  badges.push(badge_maker(name))
+  end
+  badges
+end
